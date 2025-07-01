7b69e4b63e6b8bedc98fc9747ccf585a79b00cac
ELANSURIYAA <117831904+ELANSURIYAA@users.noreply.github.com>
2025-07-01 18:48:33 +0530

    Add or update file: data_transform_pipeline.py

--- Talend_to_PySpark_Conversion/data_transform_pipeline.py
+++ Talend_to_PySpark_Conversion/data_transform_pipeline.py
@@ -1,36 +1,41 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, concat_ws
+from pyspark.sql.functions import col, count, collect_list, lit, avg
 
-# Initialize Spark Session
+# Initialize Spark session
 spark = SparkSession.builder.appName("Data Transformation Pipeline").getOrCreate()
 
 # Database connection details
 host = "<Host>"
 port = "<Port>"
 database = "<Database>"
-username = "<Username>"
+username = "<Useranme>"
 password = "<Password>"
 
 # JDBC URL
 jdbc_url = f"jdbc:postgresql://{host}:{port}/{database}"
 
-# Read data from the database
-employee_df = spark.read.format("jdbc") \n    .option("url", jdbc_url) \n    .option("dbtable", "employee") \n    .option("user", username) \n    .option("password", password) \n    .load()
+# Load data from PostgreSQL
+employee_df = spark.read.format("jdbc").option("url", jdbc_url).option("dbtable", "employee").option("user", username).option("password", password).load()
 
-# Aggregation: Group by Manager_id and concatenate names
-aggregated_df = employee_df.groupBy("Manager_id") \n    .agg(concat_ws(",", col("Name")).alias("Name_list"))
+# Aggregation: Group by Manager_id and collect names
+agg_df = employee_df.groupBy("Manager_id").agg(
+    collect_list("Name").alias("Name_list")
+)
 
-# Normalize: Split Name_list into individual names
-normalized_df = aggregated_df.selectExpr("Manager_id", "explode(split(Name_list, ',')) as Name")
+# Normalize: Split names into individual rows
+normalized_df = agg_df.withColumn("Name", col("Name_list").cast("array<string>")).select(
+    col("Manager_id"), col("Name")
+)
 
-# Join with salary data
-salary_df = spark.read.format("jdbc") \n    .option("url", jdbc_url) \n    .option("dbtable", "employee") \n    .option("user", username) \n    .option("password", password) \n    .load()
+# Load salary data
+salary_df = spark.read.format("jdbc").option("url", jdbc_url).option("dbtable", "employee").option("user", username).option("password", password).load()
 
-joined_df = normalized_df.join(salary_df.select("Id", "Salary"), normalized_df["Manager_id"] == salary_df["Id"], "inner")
+# Join normalized data with salary data
+final_df = normalized_df.join(salary_df, normalized_df.Manager_id == salary_df.Manager_id, "inner")
 
-# Write the final output to a CSV file
+# Write final output to file
 output_path = "<Filepath>"
-joined_df.write.format("csv") \n    .option("header", "true") \n    .option("delimiter", ";") \n    .save(output_path)
+final_df.write.csv(output_path, header=True)
 
-# Stop Spark Session
+# Stop Spark session
 spark.stop()
